a41ad80a08b4907e521e17fee0ce2f4b5e909d71
Gid.Lamim <gid.lamim@gmail.com>
2022-06-28 19:15:14 -0300

    add script logic

--- scripts.js
+++ scripts.js
@@ -1,24 +1,44 @@
-
-var btn = document.getElementById("addBtn");
-var inputField = document.getElementById("task");
-var ul = document.getElementById("toDoList");
+var firstInput = document.getElementById("firstNumber");
+var secondInput = document.getElementById("secondNumber")
+var submitBtn = document.getElementById("addBtn");
 var clearBtn = document.getElementById("clearBtn");
+var tryAgain = document.getElementById("tryAgain");
+var msgBox = document.getElementById("msg-Box");
+var msgField = document.getElementById("text");
 
-btn.addEventListener("click", addTask);
-clearBtn.addEventListener("click", clearList);
+submitBtn.addEventListener("click", makeItHappen);
+clearBtn.addEventListener("click", clearInput);
+tryAgain.addEventListener("click", Restart);
 
 function getInput() {
-        var input = inputField.value;
-        inputField.value = "";
-        return input;
+        var x = parseInt(firstInput.value);
+        var y = parseInt(secondInput.value);
+        var comparador = x==y? " são iguais" : " não são iguais";
+        var soma = (x + y);
+        var test1 = "maior";
+        var test2 = "maior";    
+
+        if ( soma > 10 && soma <20) {
+            test2 = "menor";
+        } else if (soma < 10){
+            test1 = "menor";
+            test2 = "menor";
+        }
+
+ 
+
+        var messagePart1 = "Os números " + x + " e " + y + " " + comparador +
+        ". Sua soma é " + soma + ", que é " + test1 + " do que 10 e " + test2 + " do que 20.";
+
+        return messagePart1;
 }
 
 
-function addTask(){
-    var li = document.createElement("li");
-    var input = getInput();
-    li.appendChild(document.createTextNode(input));
-    ul.appendChild(li);  
+function makeItHappen(){
+    var li = document.createElement("p");
+    var message = getInput();
+    li.appendChild(document.createTextNode(message));
+    msgField.appendChild(li);  
 }
 
 function clearList(){
