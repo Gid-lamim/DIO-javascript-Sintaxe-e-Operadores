09ae55c72f97935b329d1a064a325e68528413d1
Gid.Lamim <gid.lamim@gmail.com>
2022-06-28 21:58:07 -0300

    fix logic

--- scripts.js
+++ scripts.js
@@ -1,45 +1,74 @@
 var firstInput = document.getElementById("firstNumber");
 var secondInput = document.getElementById("secondNumber")
 var submitBtn = document.getElementById("addBtn");
-var clearBtn = document.getElementById("clearBtn");
 var tryAgain = document.getElementById("tryAgain");
-var msgBox = document.getElementById("msg-Box");
+var formBox = document.getElementById("formBox");
+var msgBox = document.getElementById("msgBox");
 var msgField = document.getElementById("text");
 
 submitBtn.addEventListener("click", makeItHappen);
-clearBtn.addEventListener("click", clearInput);
-tryAgain.addEventListener("click", Restart);
+tryAgain.addEventListener("click", restart);
+
+
+function restart(){
+    displayForm();
+    msgField.innerHTML = "";
+}
+
 
 function getInput() {
+
         var x = parseInt(firstInput.value);
         var y = parseInt(secondInput.value);
-        var comparador = x==y? " são iguais" : " não são iguais";
-        var soma = (x + y);
-        var test1 = "maior";
-        var test2 = "maior";    
+        var message = "";
+        console.log((firstInput.value == "")? "s":"e");
+        console.log(firstInput.value);
 
-        if ( soma > 10 && soma <20) {
-            test2 = "menor";
-        } else if (soma < 10){
-            test1 = "menor";
-            test2 = "menor";
+        if (firstInput.value != "" || secondInput.value != 0){
+            var comparador = x==y? " são iguais" : " não são iguais";
+            var soma = (x + y);
+            var test1 = "maior";
+            var test2 = "maior";    
+
+            if ( soma > 10 && soma <20) {
+                test2 = "menor";
+            } else if (soma < 10){
+                test1 = "menor";
+                test2 = "menor";
+            }
+             message = "Os números " + x + " e " + y + " " + comparador +
+            ". Sua soma é " + soma + ", que é " + test1 + " do que 10 e " + test2 + " do que 20.";
+        }else{
+            message = "Inputs are empty";    
         }
 
- 
-
-        var messagePart1 = "Os números " + x + " e " + y + " " + comparador +
-        ". Sua soma é " + soma + ", que é " + test1 + " do que 10 e " + test2 + " do que 20.";
-
-        return messagePart1;
+        return message;
 }
+
 
 
 function makeItHappen(){
-    var li = document.createElement("p");
-    var message = getInput();
-    li.appendChild(document.createTextNode(message));
-    msgField.appendChild(li);  
+    
+        
+        var p = document.createElement("p");
+        var message = getInput();
+        p.appendChild(document.createTextNode(message));
+        msgField.appendChild(p);  
+       
+        displayMsg();
+    
 }
+
+function displayMsg(){
+    formBox.className="disabledBox";
+    msgBox.className="msgBox";
+}
+
+function displayForm(){
+    formBox.className="";
+    msgBox.className="disabledBox"; 
+}
+
 
 function clearList(){
     ul.innerHTML = "";
